35b0a29b78960fad7429427e265634ae4a411d24
Lexip <lexip.ctrl@gmail.com>
2021-02-01 20:39:14 +0100

    Initialize Themes, Colors and Frontpage

---
+++ app/src/main/java/dev/lexip/hub/MainActivity.java
@@ -0,0 +1,18 @@
+package dev.lexip.hub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
