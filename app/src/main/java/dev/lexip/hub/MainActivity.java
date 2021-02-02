2f04c595f727a2d8160c84f82ae401af2b79e94f
Lexip <lexip.ctrl@gmail.com>
2021-02-02 15:09:35 +0100

    Remote Config: Implement initialization and fetching

--- app/src/main/java/dev/lexip/hub/MainActivity.java
+++ app/src/main/java/dev/lexip/hub/MainActivity.java
@@ -1,18 +1,51 @@
 package dev.lexip.hub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Remote Config: Initialize Firebase Remote Config
+        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3500)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
+        mFirebaseRemoteConfig.fetch(10);
+
+        // Remote Config: Update the displayed information
+        mFirebaseRemoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            boolean updated = task.getResult();
+                            Log.d(TAG, "Config params updated: " + updated);
+                        }
+                        /** TextView tvRomName = (TextView) findViewById(R.id.tvRomName);
+                        String s = mFirebaseRemoteConfig.getString("rom_name");
+                        tvRomName.setText(mFirebaseRemoteConfig.getString("rom_name")); **/
+                    }
+                });
+
+
     }
 }
