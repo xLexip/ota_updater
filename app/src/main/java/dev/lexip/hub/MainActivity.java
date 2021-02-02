b2ec1e1e406da4fabcc1bde060e612c2d91285a0
Lexip <lexip.ctrl@gmail.com>
2021-02-02 18:27:27 +0100

    Require an internet connecton

--- app/src/main/java/dev/lexip/hub/MainActivity.java
+++ app/src/main/java/dev/lexip/hub/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -20,25 +23,62 @@ import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        // Switch to Split Screen
-        findViewById(R.id.clSplitScreen).setVisibility(View.VISIBLE);
-        findViewById(R.id.clMain).setVisibility(View.INVISIBLE);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        ((TextView)findViewById(R.id.tvLoading)).setText("Beaming data from space...");
+        setLoadingScreen(true);
 
         // Remote Config: Initialize Firebase Remote Config
-        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 .setMinimumFetchIntervalInSeconds(3500)
                 .build();
         mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
         mFirebaseRemoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
         mFirebaseRemoteConfig.fetch(10);
+
+        // Require Internet Connection: Start Thread to make sure client stays connected to the internet
+        new Thread(){
+            public void run(){
+                while(true) {
+                    if (!getNetworkState() && findViewById(R.id.clSplitScreen).getVisibility() == View.GONE) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {}
+                    if (getNetworkState() && findViewById(R.id.clSplitScreen).getVisibility() == View.VISIBLE) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                }
+            }
+        }.start();
+
+        loadConfig();
+    }
+
+    /**
+     *  (Re-)Load the Remote Config and refresh the UI
+     */
+    public void loadConfig(){
+        if(!getNetworkState()) {
+            setLoadingScreen(true);
+            return;
+        }
+        setLoadingScreen(true);
+        ((TextView) findViewById(R.id.tvLoading)).setText("Beaming data from space...");
 
         // Remote Config: Update the displayed information
         mFirebaseRemoteConfig.fetchAndActivate()
@@ -49,19 +89,39 @@ public class MainActivity extends AppCompatActivity {
                             boolean updated = task.getResult();
                             Log.d(TAG, "Config params updated: " + updated);
                         }
-                        /** TextView tvRomName = (TextView) findViewById(R.id.tvRomName);
+                        // Update UI
+                        TextView tvRomName = (TextView) findViewById(R.id.tvRomName);
                         String s = mFirebaseRemoteConfig.getString("rom_name");
-                        tvRomName.setText(mFirebaseRemoteConfig.getString("rom_name")); **/
+                        tvRomName.setText(mFirebaseRemoteConfig.getString("rom_name"));
+
+                        final Handler handler = new Handler(Looper.getMainLooper());
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                setLoadingScreen(false);
+                            }
+                        }, 1500);
                     }
                 });
+    }
 
-        // Switch to Main Screen after delay
-        final Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                findViewById(R.id.clSplitScreen).setVisibility(View.INVISIBLE);
-                findViewById(R.id.clMain).setVisibility(View.VISIBLE);            }
-        }, 1500);
+    public void setLoadingScreen(boolean pState){
+        ((TextView)findViewById(R.id.tvLoading)).setText("Whoops!"+System.getProperty("line.separator")+"There's not even a single bit of internet over here."+System.getProperty("line.separator")+"Please check your connection.");
+        if(pState){
+            findViewById(R.id.clMain).setVisibility(View.GONE);
+            findViewById(R.id.clSplitScreen).setVisibility(View.VISIBLE);
+            return;
+        }
+        findViewById(R.id.clSplitScreen).setVisibility(View.GONE);
+        findViewById(R.id.clMain).setVisibility(View.VISIBLE);
+    }
+
+    public boolean getNetworkState(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED)
+            return true;
+        else
+            return false;
     }
 }
