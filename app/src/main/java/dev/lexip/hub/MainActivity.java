3f9145c29b1c1b883c2ad18bd4a5ba981f48b716
xLexip <lexip.ctrl@gmail.com>
2021-04-17 10:48:07 +0200

    Kill the Main Activity after launching the Update Activity
    >> Prevents update issues when returning from the Update Activity

--- app/src/main/java/dev/lexip/hub/MainActivity.java
+++ app/src/main/java/dev/lexip/hub/MainActivity.java
@@ -163,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
                                 public void onClick(View v) {
                                     Intent intent = new Intent(MainActivity.this, UpdateActivity.class);
                                     startActivity(intent);
+                                    finish();
                                 }
                             });
 
